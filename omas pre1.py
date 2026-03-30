d6572f862efbef4706a22c35754dab0d5b900a7a
omasansey <omasansey@gmail.com>
2026-03-30 12:03:57 +0100

    nnnn

--- omas pre1.py
+++ omas pre1.py
@@ -520,60 +520,59 @@
 # tim = Screen()
 # tim.exitonclick()
 
-#----------------------------------------
-import turtle
-from turtle import Turtle, Screen
-import random
-is_race_on = False
+#--------------------------------------------
+# import turtle
+# from turtle import Turtle, Screen
+# import random
+# is_game_on = False
+# #
+# # screen = Screen()
+# # screen.setup(900, 600)
+# # screen.bgcolor("black")
+#
+#
+#
+# screen = Screen()
+# screen.setup(500, 400)
+# screen.bgcolor("grey")
+# user_bet = screen.textinput(title = "make a bet", prompt = "which turtle color will win")
+# colors = ["red", "green", "yellow", "blue", "brown", "pink"]
+# y_position = [-70,-40, -10, 20, 50, 80]
+# all_turtle = []
+#
+# for turtle_index in range (0, 6):
+#     new_turtle = Turtle(shape = "triangle")
+#     new_turtle.color(colors[turtle_index])
+#     new_turtle.penup()
+#     new_turtle.goto(x = -230, y = y_position[turtle_index])
+#     all_turtle.append(new_turtle)
+#
+# if user_bet:
+#     is_game_on = True
+#
+# while is_game_on:
+#     for turtle in all_turtle:
+#         if turtle.xcor() > 230:
+#             is_game_on = False
+#             winning_color = turtle.pencolor()
+#             if winning_color == user_bet:
+#                 print (f"you won the {winning_color} is the winner")
+#             else:
+#                 print(f"you lost the {winning_color} is the winner")
+#
+#         random_movement = random.randint(1, 10)
+#         turtle.forward(random_movement)
+#
+# screen.exitonclick()
 
-screen = Screen()
-screen.setup(500, 500)
-user_bet = screen.textinput(title = "make you bet", prompt = "which color of turtle will win the race")
-colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-y_position = [-70, -40, -10, 20, 50, 80]
-all_turtles = []
+#----------day 19---------------------------
+#-----------day 20-------------------------
+#-----------day 21-------------------------
 
-for turtle_index in range (0, 6):
-    new_turtle = Turtle(shape = "turtle")
-    new_turtle.color(colors[turtle_index])
-    new_turtle.penup()
-    new_turtle.goto(x = -230, y = y_position[turtle_index])
-    all_turtles.append(new_turtle)
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 
-if user_bet:
-    is_race_on = True
+class Snake:
 
-while is_race_on:
-    for turtle in all_turtles:
-        if turtle.xcor() > 230:
-            is_race_on = False
-            winning_color = turtle.pencolor()
-            if winning_color == user_bet:
-                print (f"you won {winning_color} is the winner")
-            else:
-                print (f"you lost {winning_color} is the winner")
-
-        random_distance = random.randint(1, 10)
-        turtle.forward(random_distance)
-
-
-screen.exitonclick()
-
--------------------------------------------
---------------------------------------------
-import turtle
-from turtle import Turtle, Screen
-import random
-
-
-
-
-
-
-
-screen.exitonclick()
-
-
-
+    def __init__(self):
 
 
